3e53208a2f91bc108d72580eb8595c1b3834deac
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2020-12-01 06:47:39 +0100

    g

--- aoc1part1.py
+++ aoc1part1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-a = pd.read_csv('.././a.csv').to_numpy(dtype='int', na_value=-1)
-for t in a:
-    for b in a:
-        if t[0] + b[0] == 2020:
-            x = t[0]*b[0]
+a = pd.read_csv('../a.csv').to_numpy(dtype='int', na_value=-1)
+for t in np.nditer(a):
+    for b in np.nditer(a):
+        if t + b == 2020:
+            x = t*b
 print (x)
